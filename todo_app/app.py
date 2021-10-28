f03e06620d6f940a35c6ef7598ea8de72998d8ab
WendiF <wendi.fan@softwire.com>
2021-10-28 17:08:12 +0100

    Created list and add todos functionality

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from flask.helpers import url_for
+from .data.session_items import *
 
 from todo_app.flask_config import Config
 
@@ -8,4 +10,11 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = get_items()
+    return render_template('index.html', items = items)
+
+@app.route('/add_item', methods=['POST'])
+def add_new_item():
+    add_item(request.form.get("title"))
+    items = get_items
+    return redirect(url_for('index'))
