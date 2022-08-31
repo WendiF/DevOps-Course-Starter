b1b49d225afb2daa7919997e388908b038e62740
Wendi Fan <wendi.fan@softwire.com>
2022-08-31 06:48:53 +0100

    Fix unit tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -2,11 +2,10 @@ import pytest
 from dotenv import load_dotenv, find_dotenv
 from todo_app import app
 import os
-import requests
 import mongomock
+from pymongo import MongoClient
 
 from datetime import date
-from todo_app.data.mongo_db_client import get_items
 
 @pytest.fixture
 def client():
@@ -18,26 +17,14 @@ def client():
         with test_app.test_client() as client:
             yield client
 
-def test_index_page(monkeypatch, client):
-    monkeypatch.setattr(requests, 'get', get_items)
-    response = client.get('/')
-    assert response.status_code == 200
-    assert 'Test card' in response.data.decode() 
-
+@pytest.fixture
+def items():
+    client = MongoClient(os.getenv("MB_CONNECTION_STRING"))
+    db = client[os.getenv("DATABASE_NAME")]
+    return db.todo_app_collection
 class StubResponse():
     def __init__(self, fake_response_data):
         self.fake_response_data = fake_response_data
 
     def json(self):
         return self.fake_response_data
-    
-def get_items(url, params):
-    test_board_id = os.environ.get('BOARD_ID')
-    fake_response_data = None
-    if url == f'https://api.trello.com/1/boards/{test_board_id}/lists':
-        fake_response_data = [{
-            'id': '123abc',
-            'name': 'To Do',
-            'cards': [{'id': '456', 'name': 'Test card', 'dateLastActivity': f'{date.today()}'}]
-        }]
-    return StubResponse(fake_response_data)
--- tests/test_view_model.py
+++ tests/test_view_model.py
@@ -5,39 +5,39 @@ from todo_app.data.view_model import ViewModel
 from datetime import date, timedelta
 
 def generate_test_items(start, end, status, is_past = False):
-    return [ApiItem(id, 'Test ApiItem', status.value, date.today() - timedelta(days=1) if is_past else date.today()) for id in range(start, end + 1)]
+    return [ApiItem(id, 'Test Item', status.value, date.today() - timedelta(days=1) if is_past else date.today()) for id in range(start, end + 1)]
 
-to_do_ApiItems = []
-in_progress_ApiItems = generate_test_items(1, 1, Status.IN_PROGRESS)
-done_ApiItems = generate_test_items(2, 4, Status.DONE)
-all_ApiItems = to_do_ApiItems + in_progress_ApiItems + done_ApiItems
+to_do_items = []
+in_progress_items = generate_test_items(1, 1, Status.IN_PROGRESS)
+done_items = generate_test_items(2, 4, Status.DONE)
+all_items = to_do_items + in_progress_items + done_items
 
 @pytest.fixture
 def view_model() -> ViewModel:
-    return ViewModel(all_ApiItems)
+    return ViewModel(all_items)
 
-def test_view_model_returns_all_ApiItems(view_model: ViewModel):
-    assert view_model.ApiItems == all_ApiItems
+def test_view_model_returns_all_items(view_model: ViewModel):
+    assert view_model.items == all_items
 
-def test_view_model_returns_to_do_ApiItems(view_model: ViewModel):
-    assert view_model.to_do_ApiItems == to_do_ApiItems
+def test_view_model_returns_to_do_items(view_model: ViewModel):
+    assert view_model.to_do_items == to_do_items
 
-def test_view_model_returns_in_progress_ApiItems(view_model: ViewModel):
-    assert view_model.in_progress_ApiItems == in_progress_ApiItems
+def test_view_model_returns_in_progress_items(view_model: ViewModel):
+    assert view_model.in_progress_items == in_progress_items
 
-def test_view_model_returns_done_ApiItems(view_model: ViewModel):
-    assert view_model.done_ApiItems == done_ApiItems
+def test_view_model_returns_done_items(view_model: ViewModel):
+    assert view_model.done_items == done_items
     
-def test_view_model_returns_should_show_all_done_ApiItems_returns_true_for_fewer_than_five_tasks(view_model: ViewModel):
-    assert view_model.should_show_all_done_ApiItems == True
+def test_view_model_returns_should_show_all_done_items_returns_true_for_fewer_than_five_tasks(view_model: ViewModel):
+    assert view_model.should_show_all_done_items == True
     
-def test_view_model_returns_should_show_all_done_ApiItems_returns_false_for_more_than_five_tasks():
-    view_model = ViewModel(done_ApiItems + generate_test_items(5, 9, Status.DONE))
-    assert view_model.should_show_all_done_ApiItems == False
+def test_view_model_returns_should_show_all_done_items_returns_false_for_more_than_five_tasks():
+    view_model = ViewModel(done_items + generate_test_items(5, 9, Status.DONE))
+    assert view_model.should_show_all_done_items == False
     
-def test_view_model_returns_recent_done_ApiItems(view_model: ViewModel):
-    assert view_model.recent_done_ApiItems == done_ApiItems
+def test_view_model_returns_recent_done_items(view_model: ViewModel):
+    assert view_model.recent_done_items == done_items
     
-def test_view_model_returns_older_done_ApiItems(view_model: ViewModel):
+def test_view_model_returns_older_done_items(view_model: ViewModel):
     view_model = ViewModel(generate_test_items(10, 16, Status.DONE, True))
-    assert view_model.older_done_ApiItems == view_model.older_done_ApiItems
+    assert view_model.older_done_items == view_model.older_done_items
